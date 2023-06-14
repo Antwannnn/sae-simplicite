2904c8b7f1cb10e7f3afd41c84067b4b44146dff
Antwan <94127380+Antwannnn@users.noreply.github.com>
2023-06-14 15:13:47 +0200

    51

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,40 +1,46 @@
 package exercice;
 
-import java.util.List;
-import java.util.Map;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
+/**
+ * Une solution algorithmique qui vise à être la plus facile à comprendre et à maintenir
+ */
 public class Exercice {
-	public static String solution(String phrase, List<Character> alphabet) {
-        // Créer une map pour associer chaque lettre à une liste de mots commençant par cette lettre
-        Map<Character, List<String>> motsParLettre = new HashMap<>();
-        for (char lettre : alphabet) {
-            motsParLettre.put(lettre, new ArrayList<>());
+    /**
+     * Permet de trier une liste de mots selon un ordre donné
+     * @param texte IN : texte à trier, les mots sont séparés par des caractères qui ne sont pas des lettres ou des chiffres
+     * @param ordre IN : ordre de tri, les lettres sont uniques et peuvent être en majuscule ou minuscule
+     * @return la liste des mots triés selon l'ordre, vide si le texte ou l'ordre est vide
+     * @throws IllegalArgumentException si un des paramètres est null
+     */
+    public static List<String> solution(String texte, List<Character> ordre) throws IllegalArgumentException
+    {
+        if(texte==null || ordre==null) {
+            throw new IllegalArgumentException("Les paramètres ne peuvent pas être null"); //On vérifie que les paramètres ne sont pas null
         }
-
-        // Diviser la phrase en mots
-        String[] mots = phrase.toLowerCase().split("\\W+");
-
-        // Parcourir chaque mot et l'ajouter à la liste correspondante dans la map
-        for (String mot : mots) {
-            if (!mot.isEmpty()) {
-                char premiereLettre = mot.charAt(0);
-                if (motsParLettre.containsKey(premiereLettre)) {
-                    motsParLettre.get(premiereLettre).add(mot);
+        if(texte.isEmpty()) {
+            return List.of(""); //Si le texte ou l'ordre est vide, on retourne une liste vide
+        }
+        //Si les paramètres sont valides, on commence le tri
+        List<String> listeMotsTries = new ArrayList<>();
+        String[] arrayMotsNonTries = texte.split("[^a-zA-Z0-9]"); //On sépare le texte en mots, on enlève les caractères qui ne sont pas des lettres ou des chiffres qui séparent les mots
+        for (char lettreOrdre : ordre) {
+            //On parcourt les lettres de l'ordre
+            for (String mot : arrayMotsNonTries) {
+                //On parcourt les mots du texte
+                if (!mot.isEmpty() && mot.charAt(0) == lettreOrdre) { //Si le mot n'est pas vide et que la première lettre du mot est la lettre de l'ordre
+                    listeMotsTries.add(mot); //On ajoute le mot à la liste des mots triés, il sera trié selon l'ordre
+                    mot=""; //On vide le mot pour ne pas le réutiliser
                 }
             }
         }
-
-        // Créer une liste de mots triés en parcourant l'alphabet et concaténant les listes de mots
-        StringBuilder phraseTrie = new StringBuilder();
-        for (char lettre : alphabet) {
-            List<String> motsDeLaLettre = motsParLettre.get(lettre);
-            for (String mot : motsDeLaLettre) {
-                phraseTrie.append(mot).append(" ");
+        //On parcourt les mots restants, ceux qui ne sont pas impacté par l'ordre
+        for (String mot : arrayMotsNonTries) {
+            if (!mot.isEmpty() && !listeMotsTries.contains(mot)) {
+                listeMotsTries.add(mot);
             }
         }
-
-        return phraseTrie.toString().trim();
+        return listeMotsTries;
     }
 }
